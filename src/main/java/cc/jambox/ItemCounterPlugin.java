43f5db857ca63608141b070872f9b39964a0ed96
rihavit <rihavit.github@gmail.com>
2025-01-17 20:35:32 +0100

    Apply early return pattern on heavily nested method; use enhanced for loop

--- src/main/java/cc/jambox/ItemCounterPlugin.java
+++ src/main/java/cc/jambox/ItemCounterPlugin.java
@@ -100,17 +100,17 @@ public class ItemCounterPlugin extends Plugin
 			for (Item item: invItems) {
 				int itemId = item.getId();
 				String itemName = itemManager.getItemComposition(itemId).getName();
-				if (regex.matcher(itemName).matches()) {
-					running_total += config.countQuantity() ? item.getQuantity() : 1;
-					ItemCounter counter = itemMap.getOrDefault(regex.pattern(), null);
-					if (counter == null) {
-						counter = new ItemCounter(itemManager.getImage(itemId), itemId, regex.pattern(),
-								running_total, this, config.formatAsOsrsNumber());
-						infoBoxManager.addInfoBox(counter);
-						itemMap.put(regex.pattern(), counter);
-					}
-				}
-			}
+                if (!regex.matcher(itemName).matches()) continue;
+
+                running_total += config.countQuantity() ? item.getQuantity() : 1;
+                ItemCounter counter = itemMap.getOrDefault(regex.pattern(), null);
+                if (counter != null) continue;
+
+                counter = new ItemCounter(itemManager.getImage(itemId), itemId, regex.pattern(),
+                        running_total, this, config.formatAsOsrsNumber());
+                infoBoxManager.addInfoBox(counter);
+                itemMap.put(regex.pattern(), counter);
+            }
 			ItemCounter counter = itemMap.getOrDefault(regex.pattern(), null);
 			if (counter != null) {
 				counter.setCount(running_total);
@@ -130,12 +130,12 @@ public class ItemCounterPlugin extends Plugin
 		Item[] result = new Item[total_len];
 		int overall_idx = 0;
 		for (Item[] itemSet : itemSets) {
-			if (itemSet != null) {
-				for (int i = 0; i < itemSet.length; i++) {
-					result[overall_idx++] = itemSet[i];
-				}
-			}
-		}
+            if (itemSet == null) continue;
+
+            for (Item item : itemSet) {
+                result[overall_idx++] = item;
+            }
+        }
 		return result;
 	}
 
